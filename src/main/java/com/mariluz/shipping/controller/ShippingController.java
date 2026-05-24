2df9f4de01d7100bb6512afb3b0e8dfe965551ad
Bruno0416 <brunomcalderonv@gmail.com>
2026-05-24 02:30:14 -0400

    ResponseEntity para todos los endpoints

--- src/main/java/com/mariluz/shipping/controller/ShippingController.java
+++ src/main/java/com/mariluz/shipping/controller/ShippingController.java
@@ -4,6 +4,8 @@ import com.mariluz.shipping.dto.*;
 import com.mariluz.shipping.service.ShippingService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,29 +25,36 @@ public class ShippingController {
 
     // 1. crear un envio
     @PostMapping("/create")
-    public ShipmentResponse createShipment(
+    public ResponseEntity<ShipmentResponse> createShipment(
         @Valid @RequestBody CreateShipmentRequest request
     ) {
-        return service.createShipment(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+            service.createShipment(request)
+        );
     }
 
     // 2. cambiar estado envio (admin)
     @PutMapping("/update-status")
-    public ShipmentResponse updateShipmentStatus(
+    public ResponseEntity<ShipmentResponse> updateShipmentStatus(
         @Valid @RequestBody UpdateShipmentStatusRequest request
     ) {
-        return service.updateShipmentStatus(request);
+        return ResponseEntity.status(HttpStatus.OK).body(
+            service.updateShipmentStatus(request)
+        );
     }
 
     // 3. obtener mis envios
     @GetMapping("/my-shipments")
-    public MyShipmentsResponse getMyShipments() {
-        return service.getMyShipments();
+    public ResponseEntity<MyShipmentsResponse> getMyShipments() {
+        return ResponseEntity.ok(service.getMyShipments());
     }
 
     // 4. cancelar pedido (para conectar con sales-service)
     @PostMapping("/cancel/{shipmentId}")
-    public void cancelShipment(@Valid @PathVariable Integer shipmentId) {
+    public ResponseEntity<?> cancelShipment(
+        @Valid @PathVariable Integer shipmentId
+    ) {
         service.cancelShipment(shipmentId);
+        return ResponseEntity.noContent().build();
     }
 }
